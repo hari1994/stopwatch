6198fead82602791911ef73d795feb78543c1c2d
Kavita2599 <kavita99@netart.io>
2025-05-24 12:21:25 +0530

    added coverage and added testcase

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { TimewatchPipe } from './timewatch.pipe';
 
+let component: any;
+let fixture: any;
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+    declarations: [AppComponent, TimewatchPipe]
+  }).compileComponents();
+  fixture = TestBed.createComponent(AppComponent);
+  component = fixture.componentInstance;
+});
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it(`should have as title 'Resume_app'`, () => {
+    expect(component.title).toEqual('Resume_app');
   });
 
   it(`should have as title 'Resume_app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Resume_app');
+    component.timesValue = 0;
+    let stopwatch = "1";
+    component.stopWatch(stopwatch);
+    expect(component.timesValue).toEqual('1');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Resume_app app is running!');
-  });
 });
